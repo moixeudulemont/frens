e0bc2af4e46dd7043f6bc68cbe70142579f15909
andy <andyguachin87@gmail.com>
2024-03-27 16:43:47 -0300

    probbaaa

--- app/api/getPubs/route.js
+++ app/api/getPubs/route.js
@@ -5,6 +5,6 @@ import { NextResponse } from 'next/server';
 export async function GET() {
     await db();
     const pubs = await Pubs.find({});
-    if(pubs.length == 0) NextResponse.json({err: 'NO HAY PUBS'}).status(400);
-    return Response.json(pubs);
+    if(pubs.length == 0) return NextResponse.json({err: 'NO HAY PUBS'}).status(401);
+    return NextResponse.json(pubs);
 }
