164cd52691b6c63e716ccfb02aafc992e1c64596
andy <andyguachin87@gmail.com>
2024-03-28 12:33:00 -0300

    stilizar

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -17,7 +17,7 @@ export default async function Home() {
   const pubs = await getData();
 
   return (
-        <main className='md:px-10 p-5'>
+        <main className='md:px-10 pt-5'>
             <h1 className='md:text-5xl text-3xl font-bold text-center'>Publicaciones</h1>
             <section className="mx-auto my-5 flex flex-col gap-5 w-full md:w-7/12 justify-center items-center">
               {
--- components/Pub.jsx
+++ components/Pub.jsx
@@ -15,21 +15,26 @@ export default function Pub({ data }) {
   return (
     <article className="flex justify-center flex-col w-full bg-[rgba(255,255,255,0.1)] backdrop-blur-md shadow-lg rounded-lg">
       <div className="flex items-center justify-between py-2 px-4">
-        <div className="flex gap-3">
-          <Image
-            src={data.avatar}
-            width={50}
-            height={50}
-            className="rounded-full opacity-50"
-            sizes="(max-width: 768px) 25px, 25px"
-            alt="avatar user"
-          />
+        <div className="flex gap-3 items-center">
+          <div className="md:w-[50px] md:h-[50px] w-[35px] h-[35px]">
+            <Image
+              src={data.avatar}
+              width={50}
+              height={50}
+              className="rounded-full opacity-50"
+              alt="avatar user"
+            />
+          </div>
           <div className="flex flex-col">
-            <p className="text-xl font-bold drop-shadow">{data.author}</p>
-            <p className="">{moment(data.date).fromNow()}</p>
+            <p className="md:text-xl text-md font-bold drop-shadow">{data.author}</p>
+            <p className="text-sm md:text-md">{moment(data.date).fromNow()}</p>
           </div>
         </div>
-        <FaEllipsis color="white" size={30} className="cursor-pointer hover:animate-pulse" />
+        <FaEllipsis
+          color="white"
+          size={25}
+          className="cursor-pointer hover:animate-pulse"
+        />
       </div>
       <Image
         alt="imagen linda"
@@ -41,19 +46,35 @@ export default function Pub({ data }) {
 
       <div className="flex items-center justify-around py-3">
         <div className="flex gap-3">
-          <FaRegThumbsUp color="white" size={25} className="cursor-pointer hover:animate-pulse" />
+          <FaRegThumbsUp
+            color="white"
+            size={20}
+            className="cursor-pointer hover:animate-pulse"
+          />
           <p className="font-bold">0</p>
         </div>
         <div className="flex gap-3">
-          <FaRegThumbsDown color="white" size={25} className="cursor-pointer hover:animate-pulse" />
+          <FaRegThumbsDown
+            color="white"
+            size={20}
+            className="cursor-pointer hover:animate-pulse"
+          />
           <p className="font-bold">0</p>
         </div>
         <div className="flex gap-3">
-          <FaRegStar color="white" size={25} className="cursor-pointer hover:animate-pulse" />
+          <FaRegStar
+            color="white"
+            size={20}
+            className="cursor-pointer hover:animate-pulse"
+          />
           <p className="font-bold">0</p>
         </div>
         <div className="flex gap-3">
-          <FaRegComment color="white" size={25} className="cursor-pointer hover:animate-pulse" />
+          <FaRegComment
+            color="white"
+            size={20}
+            className="cursor-pointer hover:animate-pulse"
+          />
           <p className="font-bold">0</p>
         </div>
       </div>
