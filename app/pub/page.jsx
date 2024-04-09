1f0da0e6c830f6b40452c2dfafae302c1bd55e3a
andy <andyguachin87@gmail.com>
2024-04-09 07:47:28 -0300

    delete atropos and zustand from proyect

--- app/pub/page.jsx
+++ app/pub/page.jsx
@@ -7,7 +7,6 @@ import "moment/locale/es";
 import CommentsForm from "@/components/ComentsForm";
 import Clipboard from "@/components/Clipboard";
 import YtPlayer from "@/components/YtPlayer";
-import Prueba from "@/components/Prueba";
 
 moment.locale("es");
 
@@ -89,15 +88,13 @@ export default async function pub({ searchParams }) {
               )}
               {pub.image && (
                 <div className="w-full h-full">
-                  <Prueba>
-                    <Image
-                      src={pub.image}
-                      width={700}
-                      height={700}
-                      alt="Image of pub"
-                      className="w-full h-full rounded-lg shadow-md mt-1"
-                    />
-                  </Prueba>
+                  <Image
+                    src={pub.image}
+                    width={700}
+                    height={700}
+                    alt="Image of pub"
+                    className="w-full h-full rounded-lg shadow-md mt-1"
+                  />
                 </div>
               )}
             </div>
